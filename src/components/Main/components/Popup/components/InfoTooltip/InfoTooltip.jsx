de754f252bdade5e09b7e62882d5edee8dab966b
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-06 09:46:33 -0300

    style: rename InfoToolTip constants

--- src/components/Main/components/Popup/components/InfoTooltip/InfoTooltip.jsx
+++ src/components/Main/components/Popup/components/InfoTooltip/InfoTooltip.jsx
@@ -1,8 +1,8 @@
 import successImg from "../../../../../../images/success.svg";
 import errorImg from "../../../../../../images/error.svg";
 
-const BAD_REQUEST_ERROR = 400;
-const UNAUTHORIZED_ERROR = 401;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 
 export default function InfoTooltip({ error, signIn = false, signUpSuccess }) {
   let successMessage = "";
@@ -10,11 +10,11 @@ export default function InfoTooltip({ error, signIn = false, signUpSuccess }) {
 
   if (signUpSuccess) {
     successMessage = "Parabéns! Agora é só fazer o login.";
-  } else if (error?.status === BAD_REQUEST_ERROR) {
+  } else if (error?.status === BAD_REQUEST) {
     errorMessage = "O endereço de e-mail já está cadastrado!";
-  } else if (error?.status === UNAUTHORIZED_ERROR && signIn) {
+  } else if (error?.status === UNAUTHORIZED && signIn) {
     errorMessage = "E-mail ou senha inválida! Verifique e tente novamente.";
-  } else if (error?.status === UNAUTHORIZED_ERROR && !signIn) {
+  } else if (error?.status === UNAUTHORIZED && !signIn) {
     errorMessage =
       "Sua sessão expirou! Por favor, faça login novamente para continuar.";
   } else if (error.name === "TypeError") {
